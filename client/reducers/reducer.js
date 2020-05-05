772b33f9a98b8c0254b9eeed10964118355871b6
Mark Fusco <32991975+QuestionMark97@users.noreply.github.com>
2020-05-05 15:56:37 -0400

    Added react-redux boilerplate code

---
+++ client/reducers/reducer.js
@@ -0,0 +1,14 @@
+import * as types from '../actionTypes/actionTypes';
+
+const initialState = {
+  
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+export default reducer;
