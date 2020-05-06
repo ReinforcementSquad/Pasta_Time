837effffb5897f1011142b87766148c210189b03
jimchn <jimmychen12249@gmail.com>
2020-05-06 17:36:03 -0400

    Google OAuth is now fully functional

--- server/config/passportSetup.js
+++ server/config/passportSetup.js
@@ -1,44 +1,51 @@
 const passport = require('passport');
 const googleOAuth = require('passport-google-oauth').OAuth2Strategy;
-const keys = require('./keys')
-const db = require("../models/dbmodel.js");
+const keys = require('./keys');
+const db = require('../models/dbmodel')
 
-// passport.serializeUser((placeholder, done) => {
-//     done(null, user_placeholder)
-// });
-
-// passport.deserializeUser((id_placeholder, done) => {
-
-// });
+passport.serializeUser(function(user, done) {
+    // done(null, user.id);
+    done(null, user);
+  });
+  
+  passport.deserializeUser(function(obj, done) {
+    // Users.findById(obj, done);
+    done(null, obj);
+  });
 
 passport.use(
     new googleOAuth({
-        callbackURL: '/auth/google/redirect',
         clientID: keys.google.clientID,
-        clientSecret: keys.google.clientSecret
+        clientSecret: keys.google.clientSecret,
+        callbackURL: '/auth/google/redirect',
     }, (accessToken, refreshToken, profile, done) => {
-        process.nextTick(() => {
-        })
-        //postgres logic here
-        //if condition check to see if user exist or not
-        console.log(profile)
-        console.log('profile id:', profile.id)
-        console.log('this is:',profile.displayName)
-        db.query = (
-            `
-            WITH insert_cte AS (
-              INSERT INTO public.google (google_id, name)
-              VALUES ($1, $2)
-              ON CONFLICT (google_id) DO NOTHING
-              RETURNING google_id, name
-            )
-            SELECT google_id, name FROM insert_cte
-            UNION ALL
-            SELECT google_id, name FROM public.google
-            WHERE google_id = $1
-            LIMIT 1;
-           `
-        )
-        //else condition create new user
-     }),
-); 
+        db.findUserById(profile.id).then((id) => {
+            if (id) {
+              return done(null, profile);
+            } else {
+              db.createUser(profile.id)
+                .then((id) => {
+                  return done(null, profile);
+                });
+            }
+        });
+}));
+// console.log(profile)
+// console.log('profile id:', profile.id)
+// console.log('this is:',profile.displayName)
+// console.log('hey', res.local)
+// db.query = (
+//     `
+//     WITH insert_cte AS (
+//       INSERT INTO public.google (google_id, name)
+//       VALUES ($1, $2)
+//       ON CONFLICT (google_id) DO NOTHING
+//       RETURNING google_id, name
+//     )
+//     SELECT google_id, name FROM insert_cte
+//     UNION ALL
+//     SELECT google_id, name FROM public.google
+//     WHERE google_id = $1
+//     LIMIT 1;
+//    `
+// )
